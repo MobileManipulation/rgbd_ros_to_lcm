1bcb5464386b17eff6dba741285e786c8a17c8e0
Syler Wagner <swagner@draper.com>
2017-11-21 17:52:02 -0500

    Refactored member names to match ROS C++ conventions

--- src/lcm_republisher.cpp
+++ src/lcm_republisher.cpp
@@ -68,10 +68,10 @@ typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
 typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> ImageAndDepthSyncPolicy; 
 typedef message_filters::Subscriber<sensor_msgs::Image> ImageSubscriber; 
 
-int i = 0;
-
 class LCMRepublisher
 {
+  int i_ = 0;
+
   ros::NodeHandle nh_;
   ros::Subscriber sub_;
 
@@ -80,19 +80,16 @@ class LCMRepublisher
   // Subscriber for the Kinect Depth Map stream
   ImageSubscriber* depth_map_sub_;
   // Kinect RGB-D Signal Synchronizer for Cropping Cloud
-  message_filters::Synchronizer<ImageAndDepthSyncPolicy>* rgb_depth_sync_policy;
+  message_filters::Synchronizer<ImageAndDepthSyncPolicy>* rgb_depth_sync_policy_;
 
   ros::Publisher rgb_pub_, depth_pub_;
 
-  std::string cloud_topic, lcm_url, rgb_topic, depth_topic, lcm_channel;
-  bool subscribe_point_cloud;
+  bool debug_output_, debug_print_statements_;
+  bool compress_rgb_;
+  bool compress_depth_;
 
-
-  bool debug_output, debug_print_statements;
-  bool compress_rgb;
-  bool compress_depth;
-
-  lcm::LCM lcm;
+  lcm::LCM lcm_;
+  std::string lcm_channel_;
 
   // Compression Buffers:
   int jpeg_quality_ = 94;
@@ -105,30 +102,35 @@ class LCMRepublisher
   uint8_t* depth_compress_buf_;
   int depth_compress_buf_size_;
 
-  bot_core::image_t* rgb_lcm;
-  bot_core::image_t* depth_lcm;
+  bot_core::image_t* rgb_lcm_msg_;
+  bot_core::image_t* depth_lcm_msg_;
 
 public:
   LCMRepublisher()
   {
     ros::NodeHandle private_nh("~");
 
-    private_nh.param<std::string>("lcm_url", lcm_url, "");
-    private_nh.param<std::string>("output_lcm_channel", lcm_channel, "");
+    std::string cloud_topic, lcm_url, rgb_topic, depth_topic;
 
-    if(!lcm.good())
+
+    private_nh.param<std::string>("lcm_url", lcm_url, "");
+    private_nh.param<std::string>("output_lcm_channel", lcm_channel_, "");
+
+    if(!lcm_.good())
         ros::shutdown();
 
-    private_nh.getParam("compress_rgb", compress_rgb);
-    private_nh.getParam("subscribe_point_cloud", subscribe_point_cloud);
-    private_nh.getParam("compress_depth", compress_depth);
-    if (private_nh.getParam("debug_output", debug_output))
+    private_nh.getParam("compress_rgb", compress_rgb_);
+    private_nh.getParam("compress_depth", compress_depth_);
+    if (private_nh.getParam("debug_output", debug_output_))
     {
       depth_pub_ = private_nh.advertise<sensor_msgs::Image>("depth", 1, true);
       rgb_pub_ = private_nh.advertise<sensor_msgs::Image>("image", 1, true);
     }
     
-    private_nh.param("debug_print_statements", debug_print_statements, false);
+    private_nh.param("debug_print_statements", debug_print_statements_, false);
+
+    bool subscribe_point_cloud;
+    private_nh.getParam("subscribe_point_cloud", subscribe_point_cloud);
 
     if (subscribe_point_cloud)
     {
@@ -144,8 +146,8 @@ public:
       rgb_sub_ = new ImageSubscriber(nh_, rgb_topic, 1);
       depth_map_sub_ = new ImageSubscriber(nh_, depth_topic, 1);
 
-      rgb_depth_sync_policy = new message_filters::Synchronizer<ImageAndDepthSyncPolicy>(ImageAndDepthSyncPolicy(10), *rgb_sub_, *depth_map_sub_);
-      rgb_depth_sync_policy->registerCallback(boost::bind(&LCMRepublisher::syncCallbackLCM, this, _1, _2));
+      rgb_depth_sync_policy_ = new message_filters::Synchronizer<ImageAndDepthSyncPolicy>(ImageAndDepthSyncPolicy(10), *rgb_sub_, *depth_map_sub_);
+      rgb_depth_sync_policy_->registerCallback(boost::bind(&LCMRepublisher::syncCallbackLCM, this, _1, _2));
       ROS_WARN("Subscribed to synchronized topics: %s, %s", rgb_topic.c_str(), depth_topic.c_str());
     }
 
@@ -160,8 +162,8 @@ public:
     depth_compress_buf_size_ = 640 * 480 * sizeof(int16_t) * 4;
     depth_compress_buf_ = (uint8_t*) malloc(depth_compress_buf_size_);
 
-    rgb_lcm = new bot_core::image_t();
-    depth_lcm = new bot_core::image_t();
+    rgb_lcm_msg_ = new bot_core::image_t();
+    depth_lcm_msg_ = new bot_core::image_t();
   }
 
   ~LCMRepublisher()
@@ -237,83 +239,83 @@ public:
   {
     // create LCM message
 
-    rgb_lcm->utime = timestamp;
+    rgb_lcm_msg_->utime = timestamp;
 
-    rgb_lcm->width = rgb.cols;
-    rgb_lcm->height = rgb.rows;
-    rgb_lcm->row_stride = rgb.cols;
+    rgb_lcm_msg_->width = rgb.cols;
+    rgb_lcm_msg_->height = rgb.rows;
+    rgb_lcm_msg_->row_stride = rgb.cols;
 
-    rgb_lcm->nmetadata = 0;
+    rgb_lcm_msg_->nmetadata = 0;
 
-    rgb_lcm->row_stride = rgb.step;
+    rgb_lcm_msg_->row_stride = rgb.step;
 
-    rgb_lcm->size = rgb.cols * rgb.rows * 3;
-    rgb_lcm->data = std::vector<unsigned char>(rgb.ptr(), rgb.ptr() + rgb_lcm->size);
-    if (!compress_rgb) 
+    rgb_lcm_msg_->size = rgb.cols * rgb.rows * 3;
+    rgb_lcm_msg_->data = std::vector<unsigned char>(rgb.ptr(), rgb.ptr() + rgb_lcm_msg_->size);
+    if (!compress_rgb_)
     {
-      rgb_lcm->pixelformat = 861030210; //$ PIXEL_FORMAT_BGR
+      rgb_lcm_msg_->pixelformat = 861030210; //$ PIXEL_FORMAT_BGR
     }
     else
     {
-      int compressed_size =  rgb_lcm->height*rgb_lcm->row_stride;
-      int compression_status = jpegijg_compress_8u_rgb(rgb_lcm->data.data(), rgb_lcm->width, rgb_lcm->height, rgb_lcm->row_stride,
+      int compressed_size =  rgb_lcm_msg_->height*rgb_lcm_msg_->row_stride;
+      int compression_status = jpegijg_compress_8u_rgb(rgb_lcm_msg_->data.data(), rgb_lcm_msg_->width, rgb_lcm_msg_->height, rgb_lcm_msg_->row_stride,
        image_buf_, &compressed_size, jpeg_quality_);
 
       if (0 != compression_status) 
       {
         fprintf(stderr, "JPEG compression failed...\n");
       }
-      memcpy(&rgb_lcm->data[0], image_buf_, compressed_size);
-      rgb_lcm->size = compressed_size;
-      rgb_lcm->pixelformat = bot_core::image_t::PIXEL_FORMAT_MJPEG;      
+      memcpy(&rgb_lcm_msg_->data[0], image_buf_, compressed_size);
+      rgb_lcm_msg_->size = compressed_size;
+      rgb_lcm_msg_->pixelformat = bot_core::image_t::PIXEL_FORMAT_MJPEG;
     }
 
-    depth_lcm->utime = rgb_lcm->utime; 
+    depth_lcm_msg_->utime = rgb_lcm_msg_->utime;
 
-    depth_lcm->width = depth.cols;
-    depth_lcm->height = depth.rows;
-    depth_lcm->row_stride = depth.cols;
+    depth_lcm_msg_->width = depth.cols;
+    depth_lcm_msg_->height = depth.rows;
+    depth_lcm_msg_->row_stride = depth.cols;
 
-    depth_lcm->nmetadata = 0;
-    depth_lcm->row_stride = depth.step;
+    depth_lcm_msg_->nmetadata = 0;
+    depth_lcm_msg_->row_stride = depth.step;
 
-    if (!compress_depth) 
+    if (!compress_depth_)
     {
-      depth_lcm->size = depth.cols * depth.rows * 2;
-      depth_lcm->data = std::vector<unsigned char>(depth.ptr(), depth.ptr() + depth_lcm->size);
-      depth_lcm->pixelformat = 357; //$ PIXEL_FORMAT_BE_GRAY16
+      depth_lcm_msg_->size = depth.cols * depth.rows * 2;
+      depth_lcm_msg_->data = std::vector<unsigned char>(depth.ptr(), depth.ptr() + depth_lcm_msg_->size);
+      depth_lcm_msg_->pixelformat = 357; //$ PIXEL_FORMAT_BE_GRAY16
     }
     else
     {
-      int uncompressed_size = depth_lcm->height * depth_lcm->width * sizeof(short);
-      if (debug_print_statements)
+      int uncompressed_size = depth_lcm_msg_->height * depth_lcm_msg_->width * sizeof(short);
+      if (debug_print_statements_)
         ROS_INFO("depth uncompressed_size: %d", uncompressed_size);
       unsigned long compressed_size = depth_compress_buf_size_;
-      if (debug_print_statements)
+      if (debug_print_statements_)
         ROS_INFO("depth compressed_size: %lu", compressed_size);
-      depth_lcm->data = std::vector<unsigned char>(depth.ptr(), depth.ptr() + depth.cols * depth.rows * 2);
+      depth_lcm_msg_->data = std::vector<unsigned char>(depth.ptr(), depth.ptr() + depth.cols * depth.rows * 2);
 
-      compress2(depth_compress_buf_, &compressed_size, (const Bytef*) depth_lcm->data.data(), uncompressed_size,
+      compress2(depth_compress_buf_, &compressed_size, (const Bytef*) depth_lcm_msg_->data.data(), uncompressed_size,
         Z_BEST_SPEED);  
-      if (debug_print_statements)
+      if (debug_print_statements_)
         ROS_INFO("depth compressed successfully with zlib!");
 
-      depth_lcm->size = (int)compressed_size;
-      memcpy(&depth_lcm->data[0], depth_compress_buf_, compressed_size);
-      depth_lcm->pixelformat = -2; 
-      if (debug_print_statements)
+      depth_lcm_msg_->size = (int)compressed_size;
+      memcpy(&depth_lcm_msg_->data[0], depth_compress_buf_, compressed_size);
+      depth_lcm_msg_->pixelformat = -2;
+      if (debug_print_statements_)
         ROS_INFO("memcpy to depth_lcm message complete!");
 
     }
 
     bot_core::images_t images;
-    images.utime = rgb_lcm->utime;
+    images.utime = rgb_lcm_msg_->utime;
     images.n_images = 2;
-    images.images.push_back(*rgb_lcm);
-    images.images.push_back(*depth_lcm);
+    images.images.push_back(*rgb_lcm_msg_);
+    images.images.push_back(*depth_lcm_msg_);
     images.image_types.push_back(0); //$ LEFT = 0
 
-    if (!compress_depth) 
+    if (!compress_depth_)
     {
       images.image_types.push_back(4); //$ DEPTH_MM = 4
     }
@@ -322,10 +324,10 @@ public:
       images.image_types.push_back(6); //$ DEPTH_MM_ZIPPED = 6
       // z depth, values similar to the OpenNI format, zipped with zlib
     }
-    lcm.publish(lcm_channel, &images);
+    lcm_.publish(lcm_channel_, &images);
 
-    i++;
-    ROS_WARN("Frame %d", i);
+    i_++;
+    ROS_WARN("Frame %d", i_);
 
   }
   /**
@@ -339,7 +341,7 @@ public:
     pcl_conversions::fromPCL(cloud->header, cloud_header);
     ros::Time acquisition_time = cloud_header.stamp;
 
-    if (debug_print_statements)
+    if (debug_print_statements_)
       ROS_INFO("Point cloud acquired at %lu", (long unsigned int) cloud->header.stamp);
 
     // printf ("Cloud: width = %d, height = %d\n", cloud->width, cloud->height);
@@ -352,7 +354,7 @@ public:
       return;
     }
 
-    if (debug_print_statements)
+    if (debug_print_statements_)
       ROS_INFO("conversion to rgb and depth cv::Mat done");
 
     publishLCM((long unsigned int) cloud->header.stamp, rgb, depth);
@@ -376,12 +378,12 @@ public:
     }
     cv::Mat depth = cv_bridge::toCvShare(depth_msg, depth_msg->encoding)->image;
 
-    if (debug_print_statements)
+    if (debug_print_statements_)
       ROS_INFO("Stamp %llu", rgb_msg->header.stamp.toNSec());
 
     long unsigned int timestamp = (long unsigned int) (rgb_msg->header.stamp.toNSec() / 1e3);
 
-    if (debug_print_statements)
+    if (debug_print_statements_)
       ROS_INFO("Images acquired at %lu", timestamp);
 
     publishLCM(timestamp, rgb, depth);
