5fe6d5b40db4ea223e7c3d2bc624a88ad2d21e03
Syler Wagner <swagner@draper.com>
2017-11-21 17:39:00 -0500

    Minor formatting cleanup

--- src/lcm_republisher.cpp
+++ src/lcm_republisher.cpp
@@ -76,9 +76,9 @@ class LCMRepublisher
   ros::Subscriber sub_;
 
   // Subscriber for the Kinect RGB stream
-  ImageSubscriber *rgb_sub_;
+  ImageSubscriber* rgb_sub_;
   // Subscriber for the Kinect Depth Map stream
-  ImageSubscriber *depth_map_sub_;
+  ImageSubscriber* depth_map_sub_;
   // Kinect RGB-D Signal Synchronizer for Cropping Cloud
   message_filters::Synchronizer<ImageAndDepthSyncPolicy>* rgb_depth_sync_policy;
 
@@ -105,8 +105,8 @@ class LCMRepublisher
   uint8_t* depth_compress_buf_;
   int depth_compress_buf_size_;
 
-  bot_core::image_t * rgb_lcm;
-  bot_core::image_t * depth_lcm;
+  bot_core::image_t* rgb_lcm;
+  bot_core::image_t* depth_lcm;
 
 public:
   LCMRepublisher()
@@ -151,7 +151,8 @@ public:
 
     //$ JPEG compression parameters
     image_buf_size_ = 640 * 480 * 10;
-    if (0 != posix_memalign((void**) &image_buf_, 16, image_buf_size_)) {
+    if (0 != posix_memalign((void**) &image_buf_, 16, image_buf_size_)) 
+    {
       fprintf(stderr, "Error allocating image buffer\n");
     }
 
@@ -175,63 +176,62 @@ public:
    * @param depth     output depth image cv::Mat
    * @return        true if conversion successful
    */
-bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, cv::Mat& depth)
-{
-  if ((cloud->width * cloud->height) == 0)
-  {
-    return false; //$ return if the cloud is not dense!
-  }
-
-  try
-  {
-    sensor_msgs::Image image_msg;
-    //$ cloud to RGB image
-    pcl::toROSMsg (*cloud, image_msg); 
-    cv_bridge::CvImagePtr input_bridge;
-
-    try 
+   bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, cv::Mat& depth)
+   {
+    if ((cloud->width * cloud->height) == 0)
     {
-      input_bridge = cv_bridge::toCvCopy(image_msg, image_msg.encoding);
-      image = input_bridge->image;
+      return false; //$ return if the cloud is not dense!
     }
-    catch (cv_bridge::Exception& ex)
+
+    try
     {
-      ROS_ERROR("Error getting RGB cv::Mat from point cloud derived image message.");
+      sensor_msgs::Image image_msg;
+      //$ cloud to RGB image
+      pcl::toROSMsg (*cloud, image_msg); 
+      cv_bridge::CvImagePtr input_bridge;
+      try 
+      {
+        input_bridge = cv_bridge::toCvCopy(image_msg, image_msg.encoding);
+        image = input_bridge->image;
+      }
+      catch (cv_bridge::Exception& ex)
+      {
+        ROS_ERROR("Error getting RGB cv::Mat from point cloud derived image message.");
+        return false;
+      }
+
+    }
+    catch (std::runtime_error e)
+    {
+      ROS_ERROR_STREAM("Error in converting cloud to RGB image message: "
+        << e.what());
       return false;
     }
 
-  }
-  catch (std::runtime_error e)
-  {
-    ROS_ERROR_STREAM("Error in converting cloud to RGB image message: "
-      << e.what());
-    return false;
-  }
+    depth = cv::Mat(cloud->height, 
+      cloud->width, CV_32FC1); 
 
-  depth = cv::Mat(cloud->height, 
-    cloud->width, CV_32FC1); 
+    int i = 0;
+    int j = 0;
 
-  int i = 0;
-  int j = 0;
-
-  //$ extract RGB depth image from point cloud
-  BOOST_FOREACH (const pcl::PointXYZRGB& pt, cloud->points) 
-  {
-    depth.at<float>(j, i) = pt.z;
-
-    i++;
-    if (i == depth.cols)
+    //$ extract RGB depth image from point cloud
+    BOOST_FOREACH (const pcl::PointXYZRGB& pt, cloud->points) 
     {
-      i = 0;
-      j++;
+      depth.at<float>(j, i) = pt.z;
+
+      i++;
+      if (i == depth.cols)
+      {
+        i = 0;
+        j++;
+      }
     }
+
+    //$ convert meters to Kinect-type depth images with integer content in mm 
+    depth.convertTo(depth, CV_16UC1, 1000.0); 
+
+    return true;
   }
-
-  //$ convert meters to Kinect-type depth images with integer content in mm 
-  depth.convertTo(depth, CV_16UC1, 1000.0); 
-
-  return true;
-}
 
   void publishLCM(long unsigned int timestamp, cv::Mat rgb, cv::Mat depth)
   {
@@ -249,7 +249,8 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
 
     rgb_lcm->size = rgb.cols * rgb.rows * 3;
     rgb_lcm->data = std::vector<unsigned char>(rgb.ptr(), rgb.ptr() + rgb_lcm->size);
-    if (!compress_rgb) {
+    if (!compress_rgb) 
+    {
       rgb_lcm->pixelformat = 861030210; //$ PIXEL_FORMAT_BGR
     }
     else
@@ -258,7 +259,8 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
       int compression_status = jpegijg_compress_8u_rgb(rgb_lcm->data.data(), rgb_lcm->width, rgb_lcm->height, rgb_lcm->row_stride,
        image_buf_, &compressed_size, jpeg_quality_);
 
-      if (0 != compression_status) {
+      if (0 != compression_status) 
+      {
         fprintf(stderr, "JPEG compression failed...\n");
       }
       memcpy(&rgb_lcm->data[0], image_buf_, compressed_size);
@@ -275,7 +277,8 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
     depth_lcm->nmetadata = 0;
     depth_lcm->row_stride = depth.step;
 
-    if (!compress_depth) {
+    if (!compress_depth) 
+    {
       depth_lcm->size = depth.cols * depth.rows * 2;
       depth_lcm->data = std::vector<unsigned char>(depth.ptr(), depth.ptr() + depth_lcm->size);
       depth_lcm->pixelformat = 357; //$ PIXEL_FORMAT_BE_GRAY16
@@ -295,7 +298,7 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
       if (debug_print_statements)
         ROS_INFO("depth compressed successfully with zlib!");
 
-      depth_lcm->size =(int)compressed_size;
+      depth_lcm->size = (int)compressed_size;
       memcpy(&depth_lcm->data[0], depth_compress_buf_, compressed_size);
       depth_lcm->pixelformat = -2; 
       if (debug_print_statements)
@@ -308,14 +311,15 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
     images.n_images = 2;
     images.images.push_back(*rgb_lcm);
     images.images.push_back(*depth_lcm);
-    images.image_types.push_back( 0 ) ; //$ LEFT = 0
+    images.image_types.push_back(0); //$ LEFT = 0
 
-    if (!compress_depth) {
-      images.image_types.push_back( 4 ) ; //$ DEPTH_MM = 4
+    if (!compress_depth) 
+    {
+      images.image_types.push_back(4); //$ DEPTH_MM = 4
     }
     else
     {
-      images.image_types.push_back( 6 ) ; //$ DEPTH_MM_ZIPPED = 6
+      images.image_types.push_back(6); //$ DEPTH_MM_ZIPPED = 6
       // z depth, values similar to the OpenNI format, zipped with zlib
     }
     lcm.publish(lcm_channel, &images);
@@ -328,8 +332,7 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
    * Cloud callback method.
    * @param image_msg   message with time-synchronized RGB image and depth
    */
-   void
-   cloudCallbackLCM(const PointCloud::ConstPtr& cloud)
+   void cloudCallbackLCM(const PointCloud::ConstPtr& cloud)
    {
 
     std_msgs::Header cloud_header;
@@ -337,7 +340,7 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
     ros::Time acquisition_time = cloud_header.stamp;
 
     if (debug_print_statements)
-      ROS_INFO("Point cloud acquired at %lu",  (long unsigned int) cloud->header.stamp);
+      ROS_INFO("Point cloud acquired at %lu", (long unsigned int) cloud->header.stamp);
 
     // printf ("Cloud: width = %d, height = %d\n", cloud->width, cloud->height);
 
@@ -361,8 +364,7 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
    * Synchronized RGB and depth image callback method.
    * @param image_msg   message with time-synchronized RGB image and depth
    */
-   void
-   syncCallbackLCM(const sensor_msgs::ImageConstPtr rgb_msg, const sensor_msgs::ImageConstPtr depth_msg)
+   void syncCallbackLCM(const sensor_msgs::ImageConstPtr rgb_msg, const sensor_msgs::ImageConstPtr depth_msg)
    {
     // Converting to OpenCV Mat
     cv::Mat rgb = cv_bridge::toCvShare(rgb_msg, rgb_msg->encoding)->image;
@@ -375,12 +377,12 @@ bool cloudToImageAndDepthMat(const PointCloud::ConstPtr& cloud, cv::Mat& image, 
     cv::Mat depth = cv_bridge::toCvShare(depth_msg, depth_msg->encoding)->image;
 
     if (debug_print_statements)
-      ROS_INFO("Stamp %llu",  rgb_msg->header.stamp.toNSec());
+      ROS_INFO("Stamp %llu", rgb_msg->header.stamp.toNSec());
 
     long unsigned int timestamp = (long unsigned int) (rgb_msg->header.stamp.toNSec() / 1e3);
 
     if (debug_print_statements)
-      ROS_INFO("Images acquired at %lu",  timestamp);
+      ROS_INFO("Images acquired at %lu", timestamp);
 
     publishLCM(timestamp, rgb, depth);
   }
