e1fc6952b8247abf61dc85e7e5e6bf0fb6da54f3
Syler <syler@dexai.com>
2018-11-24 15:09:49 -0500

    Cleanup and better printouts

--- src/image_extractor.cpp
+++ src/image_extractor.cpp
@@ -23,14 +23,14 @@ class ImageExtractor
     int height;
     int num_pixels_;
 
-    unsigned char * depth_decompress_buf_;
-    unsigned char * image_decompress_buf_;
+    unsigned char * depth_read_buf_;
+    unsigned char * image_read_buf_;
     int32_t depth_size_;
     int32_t image_size_;
 
 public:
-  ImageExtractor() : depth_decompress_buf_(0),
-                     image_decompress_buf_(0)
+  ImageExtractor() : depth_read_buf_(0),
+                     image_read_buf_(0)
   {
     ros::NodeHandle private_nh("~");
 
@@ -74,12 +74,13 @@ public:
             if (i == 0) {
                 initializeDimensions(message);
             }
+            ROS_INFO("Saving frame %d", i);
             decompressAndSaveImages(message);
             i++;
         }
     }
 
-    ROS_INFO("Found %d events on %s channel", i, lcm_channel_.c_str()); 
+    ROS_INFO("Saved %d events from %s channel", i, lcm_channel_.c_str()); 
 
   }
 
@@ -93,70 +94,56 @@ public:
     width = color_image.width;
     height = color_image.height;
     num_pixels_ = width * height;
-    depth_decompress_buf_ = new unsigned char[num_pixels_ * 2];
-    image_decompress_buf_ = new unsigned char[num_pixels_ * 3];
+    depth_read_buf_ = new unsigned char[num_pixels_ * 2];
+    image_read_buf_ = new unsigned char[num_pixels_ * 3];
   }
 
   void decompressAndSaveImages(bot_core::images_t message)
   {
     int64_t timestamp = message.utime;
-    std::cout << "timestamp: " << timestamp << std::endl;
+    ROS_INFO("Timestamp: %llu", timestamp);
 
     bot_core::image_t color_image = message.images[0];
     bot_core::image_t depth_image = message.images[1];
 
-
-    bool zlib_compressed = false;
-
-    if (depth_image.pixelformat == bot_core::image_t::PIXEL_FORMAT_INVALID)
-    {
-      zlib_compressed = true;
-    }
-
     depth_size_ = depth_image.size;
     image_size_ = color_image.size;
 
-    if(depth_size_ == num_pixels_ * 2)
+    if (depth_size_ == num_pixels_ * 2)
     {
-       // printf("copying depth image\n");
-        memcpy(&depth_decompress_buf_[0], depth_image.data.data(), num_pixels_ * 2);
+        //$ copy uncompressed depth image
+        memcpy(&depth_read_buf_[0], depth_image.data.data(), num_pixels_ * 2);
     }
     else
     {
-      // printf("uncompress depth image\n");
+        //$ decompress zlib compressed depth
         unsigned long decomp_length = num_pixels_ * 2;
-        uncompress(&depth_decompress_buf_[0], (unsigned long *)&decomp_length, (const Bytef *)depth_image.data.data(), depth_size_);
+        uncompress(&depth_read_buf_[0], (unsigned long *)&decomp_length, (const Bytef *)depth_image.data.data(), depth_size_);
     }
 
-    if(image_size_ == num_pixels_ * 3)
+    if (image_size_ == num_pixels_ * 3)
     {
-      //  printf("copy color image\n");
-        memcpy(&image_decompress_buf_[0], color_image.data.data(), num_pixels_ * 3);
+        //$ copy uncompressed rgb image
+        memcpy(&image_read_buf_[0], color_image.data.data(), num_pixels_ * 3);
     }
-    else if(image_size_ > 0)
+    else if (image_size_ > 0)
     {
-      //  printf("jpeg read color image\n");
-        std::cout << "before jpeg read data" << std::endl;
-        std::cout << "image_size_: " << image_size_ << std::endl;
+        //$ decompress jpeg compressed rgb image
         size_t w = color_image.width;
         size_t h = color_image.height;
-        jpegijg_decompress_8u_rgb(color_image.data.data(), color_image.size, (unsigned char *)&image_decompress_buf_[0], w, h, w*3);
-
-        std::cout << "after jpeg read data" << std::endl;
-    }
-    else
-    {
-        memset(&image_decompress_buf_[0], 0, num_pixels_ * 3);
+        jpegijg_decompress_8u_rgb(color_image.data.data(), color_image.size, (unsigned char *)&image_read_buf_[0], w, h, w*3);
     }
 
-    cv::Mat image_mat(height, width, CV_8UC3, &image_decompress_buf_[0]); 
-    cv::cvtColor(image_mat, image_mat, CV_RGB2BGR); //$ flip RGB -> BGR channel order for saving with OpenCV
+
+    cv::Mat image_mat(height, width, CV_8UC3, &image_read_buf_[0]); 
+    //$ flip RGB -> BGR channel order for saving with OpenCV
+    cv::cvtColor(image_mat, image_mat, CV_RGB2BGR); 
 
     std::string image_name = output_folder_ + "/" + std::to_string(timestamp) + "_image.png";
     cv::imwrite(image_name, image_mat);
 
     std::string depth_name = output_folder_ + "/" + std::to_string(timestamp) + "_depth.png";
-    cv::Mat depth_mat(height, width, CV_16UC1, &depth_decompress_buf_[0]); 
+    cv::Mat depth_mat(height, width, CV_16UC1, &depth_read_buf_[0]); 
     cv::imwrite(depth_name, depth_mat);
 
   }
